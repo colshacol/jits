775d5a136d988b55bf7952359df61d70e178d344
Colton Colcleasure <cocolcleasure@MM-C03844.local>
2018-05-04 03:38:02 -0500

    Updated --help docs.

--- src/argsTool.js
+++ src/argsTool.js
@@ -2,22 +2,34 @@ import meow from 'meow'
 import emoji from 'node-emoji'
 
 const helpMessage = `
-  
-	options:
+# usage:
 
-		--file, -F        The path to the file you wish to upload.
-		--dir, -F         [no workie yet] The path to the directory you wish to upload.
+  jits [options]
 
-		--user, -U        The Github username to use for authentication.
-		--pass, -P        The Github password to use for authentication.
+# options:
 
-		--set-user, -SU   Set and persist your Github username.
-		--set-pass, -SP   Set and persist your Github password.
+  --file, -F          # The path to the file you wish to upload.
+  --dir, -F           # [no workie yet] The path to the directory you wish to upload.
 
-	examples:
+  --user, -U          # The Github username to use for authentication.
+  --pass, -P          # The Github password to use for authentication.
 
-		jits -f dist/js/bundle.js -u joedirt12 -p digit1234
+  --set-user, -SU     # Set and persist your Github username.
+  --set-pass, -SP     # Set and persist your Github password.
 
+# examples:
+
+  # Upload a file and provide authentication data.
+  jits -f dist/js/bundle.js -u joedirt12 -p digit1234
+
+  # Provide username for future usage by jits.
+  jits --set-user joedirt12
+
+  # Provide password for future usage by jits.
+  jits --set-pass digit1234
+
+  # Upload a file simply, relying on previously set authentication data.
+  jits -f dist/js/bundle.js
   
 `
 
